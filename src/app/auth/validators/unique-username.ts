be1b9d7ad16cfcc4ef31731cba8048da3c7f285b
ClaytonDewey <clay@dryadmedia.com>
2022-05-13 14:23:01 -0500

    Start work on Async Validator...

--- src/app/auth/validators/unique-username.ts
+++ src/app/auth/validators/unique-username.ts
@@ -1,2 +1,18 @@
-export class UniqueUsername {
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { AsyncValidator, AbstractControl } from "@angular/forms";
+
+@Injectable({
+    providedIn: "root"
+})
+export class UniqueUsername implements AsyncValidator {
+    constructor(private http: HttpClient) {}
+    
+    validate(control: AbstractControl) {
+        console.log('value', control.value);
+
+        return control.value;
+    }
 }
+
+// 4:11 Nasty Async Validators
