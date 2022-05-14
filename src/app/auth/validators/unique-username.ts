60cfbf6a2098303dc8d90f8fad6b2fbb560acdb4
ClaytonDewey <clay@dryadmedia.com>
2022-05-14 11:10:18 -0500

    Complet unique-username validator

--- src/app/auth/validators/unique-username.ts
+++ src/app/auth/validators/unique-username.ts
@@ -1,6 +1,8 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { AsyncValidator, AbstractControl } from "@angular/forms";
+import { map, catchError } from "rxjs/operators";
+import { of } from "rxjs";
 
 @Injectable({
     providedIn: "root"
@@ -8,11 +10,30 @@ import { AsyncValidator, AbstractControl } from "@angular/forms";
 export class UniqueUsername implements AsyncValidator {
     constructor(private http: HttpClient) {}
     
-    validate(control: AbstractControl) {
-        console.log('value', control.value);
+    validate = (control: AbstractControl) => {
+        const { value } = control;
 
-        return control.value;
-    }
+        return this.http.post<any>('https://api.angular-email.com/auth/username', {
+            username: value 
+        }).pipe(
+            map((value) => {
+                if (value.available) {
+                    return null;
+                } else {
+                    return value;
+                }
+            }),
+            catchError((err) => {
+                console.log(err)
+                if (err.error.username) {
+                    // of() = shortcut for creating new Observable
+                    return of({ nonUniqueUsername: true });
+                } else {
+                    return of({ noConnection: true });
+                }
+            })
+        )
+    };
 }
 
 // 4:11 Nasty Async Validators
