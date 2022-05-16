967a5859e6c8a7ef3ee33971910d815828e98123
ClaytonDewey <clay@dryadmedia.com>
2022-05-16 09:24:57 -0500

    Set up base input component

--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
@@ -8,6 +9,8 @@ import { Component, OnInit } from '@angular/core';
 export class InputComponent implements OnInit {
 
   constructor() { }
+  @Input() label: string;
+  @Input() control: FormControl
 
   ngOnInit(): void {
   }
