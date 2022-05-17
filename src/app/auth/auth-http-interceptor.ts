39116566ddd3a70a5f473492f4e1430d46271dfc
ClaytonDewey <clay@dryadmedia.com>
2022-05-17 12:45:34 -0500

    More auth interceptor work

--- src/app/auth/auth-http-interceptor.ts
+++ src/app/auth/auth-http-interceptor.ts
@@ -3,9 +3,11 @@ import {
     HttpEvent,
     HttpHandler,
     HttpInterceptor,
-    HttpRequest
+    HttpRequest,
+    HttpEventType
 } from "@angular/common/http";
 import { Observable } from "rxjs";
+import { tap, filter } from "rxjs/operators";
 
 @Injectable()
 export class AuthHttpInterceptor implements HttpInterceptor {
@@ -13,10 +15,17 @@ export class AuthHttpInterceptor implements HttpInterceptor {
         req: HttpRequest<any>, 
         next: HttpHandler
     ): Observable<HttpEvent<any>> {
+        // Modify or log the outgoing request
         const modifiedReq = req.clone({
             withCredentials: true
         });
 
-        return next.handle(modifiedReq);
+        return next.handle(modifiedReq).pipe(
+            filter(val => val.type === HttpEventType.Sent),
+
+            tap(val => {
+                console.log('Sent the request');
+            })
+        );
     }
 }
