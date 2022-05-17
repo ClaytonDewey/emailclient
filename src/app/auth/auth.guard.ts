e1808b37fc5c84ba0de8e1cf06b36107aac42309
ClaytonDewey <clay@dryadmedia.com>
2022-05-17 16:14:50 -0500

    Working on canLoad guard

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -6,9 +6,13 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class AuthGuard implements CanLoad {
-  canLoad(
-    route: Route,
-    segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
-  }
+    canLoad(
+        route: Route,
+        segments: UrlSegment[]
+    ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+        return new Observable((subscriber) => {
+            subscriber.next(true);
+            subscriber.complete();
+        })
+    }
 }
