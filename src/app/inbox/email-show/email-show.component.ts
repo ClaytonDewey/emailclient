d611b13ad3f187c49d99c3389e9e8d8ced8b57c0
ClaytonDewey <clay@dryadmedia.com>
2022-05-19 12:44:39 -0500

    Cancel previous email requests

--- src/app/inbox/email-show/email-show.component.ts
+++ src/app/inbox/email-show/email-show.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { EmailService } from '../email.service';
 
 @Component({
   selector: 'app-email-show',
@@ -8,11 +10,24 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class EmailShowComponent implements OnInit {
 
-    constructor(private route: ActivatedRoute) { }
+    constructor(
+        private route: ActivatedRoute,
+        private emailService: EmailService
+    ) { }
 
     ngOnInit(): void {
-        this.route.params.subscribe(({ id }) => {
-            console.log(id)
+        // this.route.params.subscribe(({ id }) => {
+        //     this.emailService.getEmail(id).subscribe(email => {
+        //         console.log(email);
+        //     })
+        // })
+
+        this.route.params.pipe(
+            switchMap(({ id }) => {
+                return this.emailService.getEmail(id);
+            })
+        ).subscribe((email) => {
+            console.log(email)
         })
     }
 }
