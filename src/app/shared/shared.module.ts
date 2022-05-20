c8a4310a259ada8178242bf24f4ee4754ed18a36
ClaytonDewey <clay@dryadmedia.com>
2022-05-20 09:26:04 -0500

    Generate Modal component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InputComponent } from './input/input.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ModalComponent } from './modal/modal.component';
 
 @NgModule({
   declarations: [
-    InputComponent
+    InputComponent,
+    ModalComponent
   ],
   imports: [
     CommonModule,
