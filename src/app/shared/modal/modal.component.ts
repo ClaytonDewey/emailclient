78caff1f2dd57b6abbc1d864377bbddd801345d5
ClaytonDewey <clay@dryadmedia.com>
2022-05-20 09:30:54 -0500

    Set up modal class template

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'app-modal',
-  templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.css']
+    selector: 'app-modal',
+    templateUrl: './modal.component.html',
+    styleUrls: ['./modal.component.css']
 })
 export class ModalComponent implements OnInit {
+    @Output() dismiss = new EventEmitter();
 
-  constructor() { }
+    constructor(private el: ElementRef) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        document.body.appendChild(this.el.nativeElement);
+    }
 
+    ngOnDestroy() {
+        this.el.nativeElement.remove();
+    }
+
+    onDismissClick() {
+        this.dismiss.emit();
+    }
 }
